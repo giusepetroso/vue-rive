b4b15a0a72614385d0afa1ce442eb3f837ea36d0
Giuse Petroso <g.petroso@gmail.com>
2021-08-17 16:40:05 +0200

    reverted master to working version

--- build/rollup.config.js
+++ build/rollup.config.js
@@ -1,21 +1,21 @@
-import commonjs from '@rollup/plugin-commonjs'; 
-import vue from 'rollup-plugin-vue'; 
-import resolve from '@rollup/plugin-node-resolve';
-import babel from '@rollup/plugin-babel';
+import commonjs from "@rollup/plugin-commonjs";
+import vue from "rollup-plugin-vue";
+import resolve from "@rollup/plugin-node-resolve";
+import babel from "@rollup/plugin-babel";
 export default {
-    input: 'src/wrapper.js', // Path relative to package.json
-    output: {
-        name: 'RiveCanvas',
-        exports: 'named',
-    },
-    plugins: [
-        commonjs(),
-        vue({
-            css: true, // Dynamically inject css as a <style> tag
-            compileTemplate: true, // Explicitly convert template to render function
-        }),
-        resolve(),
-        babel({ babelHelpers: 'bundled' })
-    ],
-    external: ['rive-canvas']
+  input: "src/wrapper.js", // Path relative to package.json
+  output: {
+    name: "RiveCanvas",
+    exports: "named",
+  },
+  plugins: [
+    commonjs(),
+    vue({
+      css: true, // Dynamically inject css as a <style> tag
+      compileTemplate: true, // Explicitly convert template to render function
+    }),
+    resolve(),
+    babel({ babelHelpers: "bundled" }),
+  ],
+  external: ["rive-js"]
 };
